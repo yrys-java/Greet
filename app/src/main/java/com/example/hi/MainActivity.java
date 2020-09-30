4f0ae76e9ac6f00cf587de1bd60339a3dbe2e69a
yrys-java <rysnurllamov26@gmail.com>
2020-09-30 18:54:15 +0600

    first push

---
+++ app/src/main/java/com/example/hi/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.hi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextTextPersonName;
+    String name = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextTextPersonName = (EditText) findViewById(R.id.editTextTextPersonName);
+        if (savedInstanceState != null) {
+            name = savedInstanceState.getString("name");
+            display((name));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("name",name);
+    }
+
+
+    public void button(View view) {
+        name = (editTextTextPersonName.getText().toString());
+
+        if (TextUtils.isEmpty(editTextTextPersonName.getText())) {
+            Context context = getApplicationContext();
+            CharSequence text = "Input your name, please!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            display(name);
+        }
+    }
+
+
+    public void display(String name) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello, "+name);
+    }
+
+}
